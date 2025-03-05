ffc8001e1ed528a4cd516d76b478bd789e94eb9d
TUNE-pixel <thinhtunnnn@gmail.com>
2025-03-05 18:15:57 +0700

    Taixiu_server

---
+++ TestIO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using SocketIOClient;  // Import thư viện SocketIO
+
+public class TestIO : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Đã kết nối thành công");
+    }
+}
